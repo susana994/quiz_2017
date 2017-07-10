4f5cf2cdc8f29223866c00086da686a08533123e
susana994 <suusii94@gmail.com>
2017-07-10 13:47:51 +0200

    Practica51

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,8 @@ router.get('/author', function(req, res, next) {
     res.render('author');
 });
 
+router.get('/help', function(req, res, next) {
+    res.render('help');
+});
+
 module.exports = router;
